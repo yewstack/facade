ceb146b5b5f5222335f4e41db0e23f34a5c5dbcf
Denis Kolodin <deniskolodin@gmail.com>
2019-07-02 07:19:01 +0900

    Spawn a minimal server

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![feature(async_await)]
+
+use failure::Error;
+use futures3::compat::{Future01CompatExt};//, Sink01CompatExt, Stream01CompatExt};
+use hyper::{Body, Response, Server};
+use hyper::service::service_fn_ok;
+use std::env;
+
+const PORT_VAR: &str = "RILLRATE_PORT";
+const PORT_DEF: &str = "12400";
+
+pub async fn main() -> Result<(), Error> {
+    let port: u16 = env::var(PORT_VAR).unwrap_or(PORT_DEF.to_string()).parse()?;
+    let addr = ([127, 0, 0, 1], port).into();
+    let make_service = || {
+        service_fn_ok(|_req| {
+            Response::new(Body::from("Hello World"))
+        })
+    };
+    let server = Server::bind(&addr)
+        .serve(make_service)
+        .compat();
+    server.await?;
+    Ok(())
 }
