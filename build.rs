531bdf960e3bd56a52eef3cb1ac2c23d38133a8c
Denis Kolodin <deniskolodin@gmail.com>
2019-07-02 08:07:16 +0900

    Pack UI to tar

--- build.rs
+++ build.rs
@@ -1,14 +1,44 @@
+use failure::{format_err, Error};
 use std::process::Command;
 
-fn main() {
-    let output = Command::new("cargo")
-        .args(&["web", "build"])
-        .current_dir("ui")
-        .output()
-        .unwrap();
+trait RunIt {
+    fn run_it(&mut self, err: &str) -> Result<(), Error>;
+}
+
+impl RunIt for Command {
+    fn run_it(&mut self, err: &str) -> Result<(), Error> {
+        let output = self.output()?;
+        if !output.status.success() {
+            let out = String::from_utf8_lossy(&output.stderr);
+            eprintln!("{}", out);
+            Err(format_err!("{}", err))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+fn run(mut command: Command, err: &str) -> Result<(), Error> {
+    let output = command.output()?;
     if !output.status.success() {
         let out = String::from_utf8_lossy(&output.stderr);
         eprintln!("{}", out);
-        panic!("Can't compile");
+        Err(format_err!("{}", err))
+    } else {
+        Ok(())
     }
+}
+
+fn main() -> Result<(), Error> {
+    Command::new("cargo")
+        .args(&["web", "deploy"])
+        .current_dir("ui")
+        .run_it("Can't compile UI")?;
+
+    Command::new("tar")
+        .args(&["-cvf", "target/ui.tar", "-C", "target/deploy", "."])
+        .current_dir("ui")
+        .run_it("Can't pack UI")?;
+
+    Ok(())
 }
