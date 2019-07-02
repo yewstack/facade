a01d94a75b5d644636efcc02272323cc4a9e2473
Denis Kolodin <deniskolodin@gmail.com>
2019-07-02 19:54:45 +0900

    Include tar archive with static files

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 use failure::{format_err, Error};
+use std::env;
+use std::path::PathBuf;
 use std::process::Command;
 
 trait RunIt {
@@ -18,25 +20,18 @@ impl RunIt for Command {
     }
 }
 
-fn run(mut command: Command, err: &str) -> Result<(), Error> {
-    let output = command.output()?;
-    if !output.status.success() {
-        let out = String::from_utf8_lossy(&output.stderr);
-        eprintln!("{}", out);
-        Err(format_err!("{}", err))
-    } else {
-        Ok(())
-    }
-}
-
 fn main() -> Result<(), Error> {
     Command::new("cargo")
         .args(&["web", "deploy"])
         .current_dir("ui")
         .run_it("Can't compile UI")?;
 
+    let out_path = PathBuf::from(env::var("OUT_DIR")?);
+    let tar_path = out_path.join("ui.tar.gz");
+    let tar_path = tar_path.to_str()
+        .ok_or_else(|| format_err!("can't create path to archive"))?;
     Command::new("tar")
-        .args(&["-cvf", "target/ui.tar", "-C", "target/deploy", "."])
+        .args(&["-cvzf", tar_path, "-C", "target/deploy", "."])
         .current_dir("ui")
         .run_it("Can't pack UI")?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 #![feature(async_await)]
 
-use failure::Error;
+use failure::{format_err, Error};
+use flate2::read::GzDecoder;
 use futures::{Future, Stream};
 use futures3::{StreamExt, TryFutureExt};
 use futures3::compat::{Compat, Future01CompatExt, Sink01CompatExt, Stream01CompatExt};
 use std::env;
+use std::io::Read;
+use std::collections::HashMap;
+use tar::Archive;
 use warp::Filter;
 use warp::filters::ws::WebSocket;
 
 const PORT_VAR: &str = "RILLRATE_PORT";
 const PORT_DEF: &str = "12400";
+const DATA: &'static [u8] = include_bytes!(concat!(env!("OUT_DIR"), "/ui.tar.gz"));
 
 pub async fn process_ws(websocket: WebSocket) -> Result<(), Error> {
     let (tx, rx) = websocket.split();
@@ -21,6 +26,23 @@ pub async fn process_ws(websocket: WebSocket) -> Result<(), Error> {
 }
 
 pub async fn main() -> Result<(), Error> {
+    let mut tar = GzDecoder::new(DATA);
+    let mut archive = Archive::new(tar);
+    let mut files = HashMap::new();
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let mut data = Vec::new();
+        if data.len() > 0 {
+            entry.read_to_end(&mut data)?;
+            let name = entry
+                .path()?
+                .to_str()
+                .ok_or_else(|| format_err!("can't get path from static srchaive"))?
+                .to_owned();
+            files.insert(name, data);
+        }
+    }
+
     let live = warp::path("live")
         .and(warp::ws2())
         .map(|ws: warp::ws::Ws2| {
