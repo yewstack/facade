07efd96776399cdf3077e722000ecec8a36cd3c6
Denis Kolodin <deniskolodin@gmail.com>
2019-08-12 02:53:37 +0900

    Replace less with sass embedded compiler

--- ui/build.rs
+++ ui/build.rs
@@ -1,5 +1,11 @@
 use failure::Error;
+use sass_rs::{compile_file, Options, OutputStyle};
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() -> Result<(), Error> {
+    let data = compile_file("./styles/styles.scss", Options::default()).unwrap();
+    let mut file = File::create("./static/styles.css")?;
+    file.write_all(data.as_bytes())?;
     Ok(())
 }
