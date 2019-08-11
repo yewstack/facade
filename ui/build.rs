41b2e97b38d737692d9d414fca2bf8d73362bf4a
Denis Kolodin <deniskolodin@gmail.com>
2019-08-12 02:59:14 +0900

    Use Compact format for SASS

--- ui/build.rs
+++ ui/build.rs
@@ -1,10 +1,12 @@
-use failure::Error;
+use failure::{err_msg, Error};
 use sass_rs::{compile_file, Options, OutputStyle};
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> Result<(), Error> {
-    let data = compile_file("./styles/styles.scss", Options::default()).unwrap();
+    let mut opts = Options::default();
+    opts.output_style = OutputStyle::Compact;
+    let data = compile_file("./styles/styles.scss", opts).map_err(err_msg)?;
     let mut file = File::create("./static/styles.css")?;
     file.write_all(data.as_bytes())?;
     Ok(())
