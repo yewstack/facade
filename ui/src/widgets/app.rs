660ef662d4021a4e321e334f908e87da576ed240
Denis Kolodin <deniskolodin@gmail.com>
2019-08-17 11:56:14 +0900

    Add navigation bar to App layout

--- ui/src/widgets/app.rs
+++ ui/src/widgets/app.rs
@@ -30,19 +30,32 @@ impl Widget for Model {
     }
 
     fn main_view(&self) -> View<Self> {
+        let mut drawer_style = String::new();
+        drawer_style.push_str("height: 100vh;");
+        drawer_style.push_str("top: 0px;");
+        drawer_style.push_str("max-height: calc(100% - 36px);");
+        drawer_style.push_str("transform: translateX(0%);");
+        drawer_style.push_str("width: 256px;");
+        let mut app_bar_style = String::new();
+        app_bar_style.push_str("margin-top: 0px;");
+        app_bar_style.push_str("transform: translateY(0px);");
+        app_bar_style.push_str("left: 256px;");
+        app_bar_style.push_str("right: 0px;");
         html! {
             <div class="f-application">
                 <div class="f-application--wrap">
-                    <div class="f-navigation-drawer">
-                        <div class="f-navigation-drawer_border" />
-                        <div class="f-navigation-drawer_content">
+                    <div class="f-navigation-drawer f-navigation-drawer--fixed" style=drawer_style>
+                        <div class="f-navigation-drawer__content">
                             <widgets::List: list=self.app.navigation_drawer.clone() />
                         </div>
+                        <div class="f-navigation-drawer__border" />
                     </div>
-                    <div class="app-bar">
+                    <div class="f-app-bar f-toolbar" style=app_bar_style>
                     </div>
-                    <div class="content">
+                    <div class="f-content">
                         <widgets::Container: container=self.app.content.clone() />
+                    </div>
+                    <div class="f-footer">
                     </div>
                 </div>
             </div>
